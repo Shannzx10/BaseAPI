cf2b4acf5c55e493336f74cafae26d4b7791fce7
Shannzx10 <134748195+Shannzx10@users.noreply.github.com>
2024-06-17 11:25:40 +0700

    Add files via upload

--- schema/config.js
+++ schema/config.js
@@ -1,15 +1,15 @@
 const options = {
-  name: "HitamAPIs",
-  developer: "@sayuran.vip",
+  name: "Shannz-APIs",
+  developer: "Shannz",
   port: 3000,
-  webName: "HitamAPIs Playground",
-  description: "Simple Base Rest API",
+  webName: "Shannz-APIs Playground",
+  description: "Ambil Semuanya Hanya Dengan Result Json.",
   favicon:
     "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Black.png/220px-Black.png",
 };
 
 const host = {
-  BASE_URL: "https://api-flax-chi-68.vercel.app", // Ganti dengan URL yang sesuai
+  BASE_URL: "https://shannz-api.vercel.app", // Ganti dengan URL yang sesuai
   // Contoh: https://domain.com
 };
 
